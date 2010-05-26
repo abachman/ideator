2297857528b3f4cf566fb7ec84f06113b4bdfcfd
Adam Bachman <adam.bachman@gmail.com>
2010-05-26 13:11:10 -0400

    added sass and datamapper

--- application.rb
+++ application.rb
@@ -1,13 +1,31 @@
-require 'haml'
-require 'json'
-
+# require 'haml'
 require 'sinatra/base'
+require 'models'
 
 class IdeatorApp < Sinatra::Base
+  set :environment, 'development'
+
+  ## CONFIGURATION
+  configure :development do
+    DataMapper.setup(:default, 'postgres://localhost/ideator_dev')
+
+    DataMapper::Logger.new(STDOUT, :debug)
+  end
+
+  configure :production do
+    DataMapper.setup(:default, ENV['DATABASE_URL'])
+  end
+
   set :sessions, true
 
   get '/' do
     haml :index
+  end
+
+  # SASS stylesheet
+  get '/stylesheets/style.css' do
+    header 'Content-Type' => 'text/css; charset=utf-8'
+    sass :style
   end
 end
 
